df13f0473e352ef7ecf1795c6c73566a717bb786
Nicolas THIERION <nicolasthierion@outlook.fr>
2020-12-05 21:34:59 +0100

    fix(core): fixes #18

--- packages/core/commons/src/annotation/factory/annotation.factory.spec.ts
+++ packages/core/commons/src/annotation/factory/annotation.factory.spec.ts
@@ -22,83 +22,89 @@ describe('AnnotationFactory', () => {
         setupTestingWeaverContext();
     });
 
-    describe('method "create"', () => {
-        describe('given any annotation', () => {
-            it('should return a decorator with the same name', () => {
-                AClass = factory.create(AClassStub);
+    describe('.create(<AnnotationStub>)', () => {
+        it('should return a decorator with the same name', () => {
+            AClass = factory.create(AClassStub);
 
-                expect(AClass.name).toEqual('AClass');
-                expect(AClass('0', 0).name).toEqual('AClass');
-            });
-
-            it(`should assign a the annotation's groupId to the factory's groupId`, () => {
-                const _AClass = factory.create(AClassStub);
-
-                expect(_AClass.groupId).toEqual(FACTORY_GROUP_TEST_ID);
-            });
-        });
-        describe('given a class annotation', () => {
-            it('should return a class decorator', () => {
-                AClass = factory.create(AClassStub);
-
-                const annotation = AClass('0', 0);
-                expect(annotation).toEqual(jasmine.any(Function));
-                const ctor = (annotation as any)(class A {});
-                expect(ctor).toEqual(jasmine.any(Function));
-                expect(ctor.name).toEqual('A');
-            });
-
-            it('should keep the class instance type', () => {
-                AClass = factory.create(AClassStub);
-
-                @AClass('', 0)
-                class A {
-                    someProp: any;
-                }
-
-                expect(new A()).toEqual(jasmine.any(A));
-            });
-
-            it(`should keep the class static properties`, () => {
-                AClass = factory.create(AClassStub);
-
-                class A {
-                    someProp: any;
-                    static someStaticProp = 'someStaticProp';
-                }
-
-                const enhancedA = (AClass('', 0)(A) as any) as typeof A;
-
-                expect(A.someStaticProp).toBeTruthy();
-                expect(enhancedA.someStaticProp).toEqual(A.someStaticProp);
-            });
+            expect(AClass.name).toEqual('AClass');
         });
 
-        describe('given a property annotation', () => {
-            it('should return a property decorator', () => {
-                AProperty = factory.create(APropertyStub);
+        it(`should assign a the annotation's groupId to the factory's groupId`, () => {
+            const _AClass = factory.create(AClassStub);
 
-                const annotation = AProperty('0', 0);
-                expect(annotation).toEqual(jasmine.any(Function));
-                const descriptor = (annotation as any)(new (class A {})(), 'propName');
-                expect(descriptor).toEqual(jasmine.any(Object));
-                expect(descriptor.enumerable).toEqual(true);
-                expect(descriptor.configurable).toEqual(true);
-            });
+            expect(_AClass.groupId).toEqual(FACTORY_GROUP_TEST_ID);
+        });
+    });
 
-            it('should not alter the property', () => {
-                AProperty = factory.create(APropertyStub);
+    describe('.create(name, <AnnotationStub>)', () => {
+        it('should return a decorator with the given name', () => {
+            AClass = factory.create('AClass', (): ClassDecorator => undefined);
 
-                class A {
-                    @AProperty()
-                    someProp: any;
-                }
+            expect(AClass.name).toEqual('AClass');
+        });
+    });
 
-                const a = new A();
-                expect(a.someProp).toEqual(undefined);
-                a.someProp = 'somePropValue';
-                expect(a.someProp).toEqual('somePropValue');
-            });
+    describe('.create(<ClassAnnotationStub>)', () => {
+        it('should return a class decorator', () => {
+            AClass = factory.create(AClassStub);
+
+            const annotation = AClass('0', 0);
+            expect(annotation).toEqual(jasmine.any(Function));
+            const ctor = (annotation as any)(class A {});
+            expect(ctor).toEqual(jasmine.any(Function));
+            expect(ctor.name).toEqual('A');
+        });
+
+        it('should keep the class instance type', () => {
+            AClass = factory.create(AClassStub);
+
+            @AClass('', 0)
+            class A {
+                someProp: any;
+            }
+
+            expect(new A()).toEqual(jasmine.any(A));
+        });
+
+        it(`should keep the class static properties`, () => {
+            AClass = factory.create(AClassStub);
+
+            class A {
+                someProp: any;
+                static someStaticProp = 'someStaticProp';
+            }
+
+            const enhancedA = (AClass('', 0)(A) as any) as typeof A;
+
+            expect(A.someStaticProp).toBeTruthy();
+            expect(enhancedA.someStaticProp).toEqual(A.someStaticProp);
+        });
+    });
+
+    describe('.create(<PropertyAnnotationStub>)', () => {
+        it('should return a property decorator', () => {
+            AProperty = factory.create(APropertyStub);
+
+            const annotation = AProperty('0', 0);
+            expect(annotation).toEqual(jasmine.any(Function));
+            const descriptor = (annotation as any)(new (class A {})(), 'propName');
+            expect(descriptor).toEqual(jasmine.any(Object));
+            expect(descriptor.enumerable).toEqual(true);
+            expect(descriptor.configurable).toEqual(true);
+        });
+
+        it('should not alter the property', () => {
+            AProperty = factory.create(APropertyStub);
+
+            class A {
+                @AProperty()
+                someProp: any;
+            }
+
+            const a = new A();
+            expect(a.someProp).toEqual(undefined);
+            a.someProp = 'somePropValue';
+            expect(a.someProp).toEqual('somePropValue');
         });
     });
 });
---
+++ packages/fetch/annotations/get.annotation.spec.ts
@@ -0,0 +1,11 @@
+// import { FetchClient } from './fetch.annotation';
+
+// TODO
+// fdescribe('@Get() annotation', () => {
+//     @FetchClient({
+//         host: 'localhost:8080',
+//         url: '/',
+//         respond: '',
+//     })
+//     class FetchResource {}
+// });
